1c6df308a8fc182b2c9f94140e50e1fa8d1f24fc
unknown <aswinitej27@gmail.com>
2023-11-04 22:08:09 +0530

    commits done

--- src/web-application/DH.py
+++ src/web-application/DH.py
@@ -26,7 +26,7 @@ def generate_private_key(length):
 		#TODO: Can use Crypto library hash functions
 		hex_key = binascii.b2a_hex(os.urandom(_bytes))
 		#Convert to denary format
-		_rand = int(hex_key.encode('hex'),16)
+		_rand = int(hex_key.hex(), 16)
 	#Update object
 	private_key = _rand
 	return private_key
@@ -39,7 +39,7 @@ def generate_public_key(private_key):
 #Secret key = public key ^ private key % q
 def generate_secret(private_key, public_key):
 	#Formula
-	secret = pow(long(public_key), long(private_key), prime)
+	secret = pow(int(public_key), int(private_key), prime)
 	try:
 		secret_bytes = secret.to_bytes(
 			shared_secret.bit_length() // 8 + 1, byteorder="big")
